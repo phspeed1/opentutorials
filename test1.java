2d75924daa91eaf9574e23433bfb5d58625ff03a
LG_ParkHonSeark <phspeed1@gmail.com>
2023-01-09 19:37:46 +0900

    프로젝트010

--- test1.java
+++ test1.java
@@ -2,9 +2,9 @@
 public class test1{
     public static void main(String[] args) {
         String str = "";
-        System.out.println("일상적인-수정");
-        System.out.println("일상적인-추가");
-        System.out.println("실험적인-수정");
-        System.out.println("실험적인-추가");
+        System.out.println("test-1");
+
+        System.out.println("test-2");
+
     }
 }
