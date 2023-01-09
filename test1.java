58ec866d265e0db3404b617df4fbdd9185ae925c
LG_ParkHonSeark <phspeed1@gmail.com>
2023-01-09 19:19:51 +0900

    일상적인 변경010

--- test1.java
+++ test1.java
@@ -2,7 +2,8 @@
 public class test1{
     public static void main(String[] args) {
         String str = "";
-        System.out.println("test1");
+        System.out.println("일상적인-수정");
+        System.out.println("일상적인-추가");
         System.out.println("test1");
     }
 }
