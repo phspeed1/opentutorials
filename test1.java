92c62637da9695701db60e90e7105e92dca40f0d
LG_ParkHonSeark <phspeed1@gmail.com>
2023-01-09 19:22:05 +0900

    실험적인 변경010

--- test1.java
+++ test1.java
@@ -3,6 +3,7 @@ public class test1{
     public static void main(String[] args) {
         String str = "";
         System.out.println("test1");
-        System.out.println("test1");
+        System.out.println("실험적인-수정");
+        System.out.println("실험적인-추가");
     }
 }
