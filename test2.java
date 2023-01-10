06d5739d5aacd5e6a059c199a106cf00c16cbaaa
LG_ParkHonSeark <phspeed1@gmail.com>
2023-01-10 18:03:27 +0900

    test2.java 최초 생성 및 추가 커밋

---
+++ test2.java
@@ -0,0 +1,9 @@
+
+public class test2{
+    public static void main(String[] args) {
+        String str = "test2.java";
+        System.out.println("test2.java");
+    }
+    // comment 010
+    // 일상적 추가
+}
